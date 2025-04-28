0f48843db92ae164fc82cc01d5c88cf44d673bf2
Mishal <mishalalexander20@gmail.com"n>
2025-04-29 01:58:45 +0530

    added code to handle login, logout and check routes
    Signed-off-by: Mishal <mishalalexander20@gmail.com"n>

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -7,7 +7,7 @@ authRoutes.post('/register', register)
 
 authRoutes.post('/login', login)
 
-authRoutes.post('/logout', logout)
+authRoutes.get('/logout', logout)
 
 authRoutes.get('/check', check)
 

9da2759907a0b6dad7c15067f4a879d197eefc0f
Mishal <mishalalexander20@gmail.com"n>
2025-04-29 01:36:25 +0530

    login route and controller
    Signed-off-by: Mishal <mishalalexander20@gmail.com"n>

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -14,7 +14,7 @@ export const register = async (req, res) => {
                 email
             }
         });
-        // if yes, then stop user from registering and throw an error
+        // if yes, then stop user from registering and return an error
         if (existingUser) {
             return res.status(400).json({
                 error: "User already registered."
@@ -69,7 +69,65 @@ export const register = async (req, res) => {
 
 }
 
-export const login = async (req, res) => { }
+export const login = async (req, res) => {
+    // get the required details from request
+    const { email, password } = req.body;
+    try {
+        // check whether the user exists by querying the dv with user email
+        const existingUser = await db.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        // if not found then return an error
+        if (!existingUser) {
+            return res.status(404).json({
+                error: "User not found"
+            })
+        }
+
+        // check whether the passwords match by comparing the hash of the password user gave with the one stored in db
+        const isPasswordMatch = await bcrypt.compare(password, existingUser.password);
+
+        // return an error if the password hash doesn't match
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                error: "Invalid credentials!"
+            })
+        }
+
+        // create a new token to be given when user logs in
+        const token = jwt.sign({ id: existingUser.id }, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
+
+        // configure cookies to be sent on success using JWT token we created above with its expiry
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+        })
+
+        // return 200 once the user is created successfully
+        res.status(200).json({
+            message: "User logged in successfully",
+            user: {
+                id: existingUser.id,
+                email: existingUser.email,
+                name: existingUser.name,
+                role: existingUser.role,
+                image: existingUser.image
+            }
+        })
+    } catch (error) {
+        console.log("Error while logging in the user");
+        res.status(500).json({
+            error: "Error while logging the user in"
+        })
+    }
+}
 
 export const logout = async (req, res) => { }
 
